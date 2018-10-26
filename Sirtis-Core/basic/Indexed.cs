2150f18c6d359f308c5998855f5972a9becd9b20
Kelvys Boniek Pantaleão <contato@kelvysb.com>
2018-10-26 00:13:46 -0300

    Added command line application.

---
+++ Sirtis-Core/basic/Indexed.cs
@@ -0,0 +1,144 @@
+﻿/*
+Copyright 2018 Kelvys B. Pantaleão
+
+This file is part of Sirtis Indexer
+
+Sirtis Indexer Is free software: you can redistribute it And/Or modify
+it under the terms Of the GNU General Public License As published by
+the Free Software Foundation, either version 3 Of the License, Or
+(at your option) any later version.
+
+This program Is distributed In the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty Of
+MERCHANTABILITY Or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License For more details.
+
+You should have received a copy Of the GNU General Public License
+along with this program.  If Not, see <http://www.gnu.org/licenses/>.
+
+Este arquivo é parte Do programa Sirtis Indexer
+
+Sirtis Indexer é um software livre; você pode redistribuí-lo e/ou 
+modificá-lo dentro dos termos da Licença Pública Geral GNU como 
+publicada pela Fundação Do Software Livre (FSF); na versão 3 da 
+Licença, ou(a seu critério) qualquer versão posterior.
+
+Este programa é distribuído na esperança de que possa ser  útil, 
+mas SEM NENHUMA GARANTIA; sem uma garantia implícita de ADEQUAÇÃO
+a qualquer MERCADO ou APLICAÇÃO EM PARTICULAR. Veja a
+Licença Pública Geral GNU para maiores detalhes.
+
+Você deve ter recebido uma cópia da Licença Pública Geral GNU junto
+com este programa, Se não, veja <http://www.gnu.org/licenses/>.
+
+'GitHub: https://github.com/Kelvysb/Sirtis-Indexer
+*/
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SirtisIndexer.basic
+{
+
+    public class IndexedFolder
+    {
+
+        #region Functions
+        public override string ToString()
+        {
+            string strReturn;
+
+            try
+            {
+                strReturn = Path + " => " + (Char)13;
+
+                foreach (IndexedFile file in Files)
+                {
+                    strReturn = strReturn + (Char)9 + file.ToString() + (Char)13;
+                }
+
+                return strReturn;
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+        #endregion
+
+        #region Properties
+        public DateTime Tiemstamp { get; set; }
+        public string Path { get; set; }
+        public List<IndexedFile> Files { get; set; }
+        public List<IndexedItem> Itens { get; set; }
+        #endregion
+    }
+
+    public class IndexedFile
+    {
+
+        #region Functions
+        public override string ToString()
+        {
+            string strReturn;
+
+            try
+            {
+                strReturn = Name  + " => " + (Char)13;
+
+                foreach (IndexedItem item in Items)
+                {
+                    strReturn = strReturn + (Char)9 + item.ToString() + (Char)13;
+                }
+                return strReturn;
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+        #endregion
+
+        #region Properties
+        public string Alias { get; set; }
+        public string Extension { get; set; }
+        public string Name { get; set; }
+        public string Path { get; set; }
+        public List<IndexedItem> Items { get; set; }
+        public string Raw { get; set; }
+        #endregion
+    }
+
+    public class IndexedItem
+    {
+
+        #region Functions
+        public override string ToString()
+        {
+            try
+            {
+                return Flat + " => " + Value;
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+        #endregion
+
+        #region Properties
+        public DateTime Tiemstamp { get; set; }
+        public string Alias { get; set; }
+        public string Flat { get; set; }
+        public string Value { get; set; }
+        public int Index { get; set; }
+        public bool Modified { get; set; }
+        public IndexedFile ParentFile { get; set; }
+        #endregion
+    }
+
+
+}
---
+++ Sirtis-Shell/SirtisShell.cs
@@ -0,0 +1,427 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SirtisIndexer.basic;
+using SirtisIndexer.layers.backend;
+
+namespace Sirtis_Shell
+{
+    class SirtisShell
+    {
+
+        #region Declarations
+        private static bool blnActive = true;
+        #endregion
+
+        #region Constructor
+
+        #endregion
+
+        #region Main
+        static void Main(string[] args)
+        {
+            string strInput;
+
+            Console.Title = "Sirtis indexer";
+
+            try
+            {
+                if (args.Count() > 0)
+                {
+                    Folder(args.ToList());
+                }
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error: " + ex.Message);
+            }
+
+            while (blnActive)
+            {
+                try
+                {
+
+                    if (SirtisIndexerBO.Instance.IndexedFolder == null)
+                    {
+                        Console.WriteLine("Insert folder:");
+                        strInput = Console.ReadLine();
+
+                        if (strInput.Trim() != "")
+                        {
+                            Folder(args.ToList());
+                        }
+                    }
+                    else
+                    {
+                        Commands();
+                    }
+
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("Error: " + ex.Message);
+                }
+            }
+        }
+        #endregion
+
+        #region Functions
+        private static void Commands()
+        {
+
+            string strInput;
+
+            try
+            {
+
+                strInput = Console.ReadLine();
+
+                if (CommandsSet.Help(strInput))
+                {
+                    Help();
+                }
+                else if (CommandsSet.All(strInput))
+                {
+                    Listall(GetParameters(strInput));
+                }
+                else if (CommandsSet.Find(strInput))
+                {
+                    Find(GetParameters(strInput));
+                }
+                else if (CommandsSet.FindIndex(strInput))
+                {
+                    FindIndex(GetParameters(strInput));
+                }
+                else if (CommandsSet.Folder(strInput))
+                {
+                    Folder(GetParameters(strInput));
+                }
+                else if (CommandsSet.Exit(strInput))
+                {
+                    blnActive = false;
+                }
+                else
+                {
+                    Console.WriteLine("Unknow command, type help for a command list.");
+                }
+
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+        private static void Find(List<string> args)
+        {
+
+            bool blnDetails;
+            string strKey;
+            List<IndexedItem> objResult;
+
+            try
+            {
+
+                objResult = new List<IndexedItem>();
+
+                if (args.Count() > 0)
+                {
+
+                    strKey = args.First();
+
+                    blnDetails = false;
+                    if (args.Count() > 1)
+                    {
+                        if (args.Last().Trim().ToUpper() == "-D")
+                        {
+                            blnDetails = true;
+                        }
+                    }
+
+                    //Filter flat
+                    objResult.AddRange(SirtisIndexerBO.Instance.IndexedFolder.Itens.FindAll(item => item.Flat.StartsWith(strKey.Trim(), StringComparison.InvariantCultureIgnoreCase)));
+
+                    if (objResult.Count() > 0)
+                    {
+                        foreach (IndexedItem item in objResult)
+                        {
+                            FormatKey(item, blnDetails);
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine("No results");
+                    }
+
+                }
+
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+        private static void FindIndex(List<string> args)
+        {
+
+            bool blnDetails;
+            int intIndex;
+            List<IndexedItem> objResult;
+
+            try
+            {
+
+                objResult = new List<IndexedItem>();
+
+                if (args.Count() > 0)
+                {
+
+                     if(int.TryParse(args.First(), out intIndex))
+                    {
+                        blnDetails = false;
+                        if (args.Count() > 1)
+                        {
+                            if (args.Last().Trim().ToUpper() == "-D")
+                            {
+                                blnDetails = true;
+                            }
+                        }
+
+                        FormatKey(SirtisIndexerBO.Instance.IndexedFolder.Itens[intIndex], blnDetails);
+
+                    }
+                    else
+                    {
+                        Console.WriteLine("Invalid index.");
+                    }
+                }
+
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+        private static void Listall(List<string> args)
+        {
+
+            bool blnDetails;
+
+            try
+            {
+                blnDetails = false;
+                if (args.Count() > 0)
+                {
+                    if (args.First().Trim().ToUpper() == "-D")
+                    {
+                        blnDetails = true;
+                    }
+                }
+
+                foreach (IndexedItem item in SirtisIndexerBO.Instance.IndexedFolder.Itens)
+                {
+                    FormatKey(item, blnDetails);
+                }
+
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+        private static void FormatKey(IndexedItem p_objItem, bool p_blnDetail)
+        {
+            try
+            {
+
+
+                Console.WriteLine(p_objItem.Flat + " = " + p_objItem.Value);
+
+                if (p_blnDetail)
+                {
+
+                    Console.WriteLine(" File: " + p_objItem.ParentFile.Name);
+                    Console.WriteLine(" File index: " + p_objItem.ParentFile.Items.IndexOf(p_objItem));
+                    Console.WriteLine(" Global index: " + SirtisIndexerBO.Instance.IndexedFolder.Itens.IndexOf(p_objItem));
+
+                }
+
+                Console.WriteLine("");
+
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+
+        }
+
+        private static void Folder(List<string> args)
+        {
+            try
+            {
+
+                if (args.Count() > 0)
+                {
+                    SirtisIndexerBO.Instance.ProcessFolderSync(args.First());
+                    FolderInfo();
+                }
+                else
+                {
+                    Console.WriteLine("Inform a folder path.");
+                }
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+
+        }
+
+        private static void Help()
+        {
+            try
+            {
+
+                Console.WriteLine("Load a folder: ");
+                Console.WriteLine(" folder \"folder path\"");
+                Console.WriteLine("");
+
+                Console.WriteLine("Find a key: ");
+                Console.WriteLine(" find \"key\"");
+                Console.WriteLine("     -d -> detailed list.");
+                Console.WriteLine("");
+
+                Console.WriteLine("List all keys: ");
+                Console.WriteLine(" all");
+                Console.WriteLine("     -d -> detailed list.");
+                Console.WriteLine("");
+
+                Console.WriteLine("Find a value: ");
+                Console.WriteLine(" value \"value\"");
+                Console.WriteLine("     -d -> detailed list.");
+                Console.WriteLine("");
+
+                Console.WriteLine("Find a item by global index: ");
+                Console.WriteLine(" index \"index\"");
+                Console.WriteLine("     -d -> detailed list.");
+                Console.WriteLine("");
+
+                Console.WriteLine("Set a key: ");
+                Console.WriteLine(" set \"key\"");
+                Console.WriteLine("");
+
+                Console.WriteLine("Set a key by global index: ");
+                Console.WriteLine(" setidx \"index\"");
+                Console.WriteLine("");
+
+                Console.WriteLine("information of a key: ");
+                Console.WriteLine(" info \"key\"");
+                Console.WriteLine("");
+
+                Console.WriteLine("information of a key by global index: ");
+                Console.WriteLine(" infoidx \"index\"");
+                Console.WriteLine("");
+
+                Console.WriteLine("save modifications: ");
+                Console.WriteLine(" save");
+                Console.WriteLine("");
+
+                Console.WriteLine("help: ");
+                Console.WriteLine(" help");
+                Console.WriteLine(" h");
+                Console.WriteLine(" ?");
+                Console.WriteLine("");
+
+                Console.WriteLine("exit: ");
+                Console.WriteLine(" exit");
+                Console.WriteLine(" quit");
+                Console.WriteLine(" q");
+
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+        private static void FolderInfo()
+        {
+            try
+            {
+
+                if (SirtisIndexerBO.Instance.IndexedFolder != null)
+                {
+                    Console.WriteLine("Loaded folder: " + SirtisIndexerBO.Instance.IndexedFolder.Path);
+                    Console.WriteLine("Files: " + SirtisIndexerBO.Instance.IndexedFolder.Files.Count());
+                    Console.WriteLine("Itens: " + SirtisIndexerBO.Instance.IndexedFolder.Itens.Count());
+                }
+                else
+                {
+                    Console.WriteLine("No Folder Loaded.");
+                }
+
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+
+        private static List<string> GetParameters(string p_strCommand)
+        {
+            List<string> strReturn;
+
+            try
+            {
+                strReturn = new List<string>();
+
+                strReturn = p_strCommand.Split(' ').ToList();
+
+                strReturn.RemoveAt(0);
+
+                return strReturn;
+
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+        #endregion
+    }
+
+
+    public class CommandsSet
+    {
+
+        private static List<string> cmdHelp = new List<string>(new string[] { "HELP", "H", "?" });
+        public static Boolean Help(string p_cmdCommand) { return cmdHelp.Contains(p_cmdCommand.Trim().ToUpper().Split(' ').First()); }
+
+        private static List<string> cmdExit = new List<string>(new string[] { "EXIT", "QUIT" });
+        public static Boolean Exit(string p_cmdCommand) { return cmdExit.Contains(p_cmdCommand.Trim().ToUpper().Split(' ').First()); }
+
+        private static List<string> cmdFind = new List<string>(new string[] { "FIND" });
+        public static Boolean Find(string p_cmdCommand) { return cmdFind.Contains(p_cmdCommand.Trim().ToUpper().Split(' ').First()); }
+
+        private static List<string> cmdFindIndex = new List<string>(new string[] { "FINDIDX" });
+        public static Boolean FindIndex(string p_cmdCommand) { return cmdFindIndex.Contains(p_cmdCommand.Trim().ToUpper().Split(' ').First()); }
+
+        private static List<string> cmdall = new List<string>(new string[] { "ALL" });
+        public static Boolean All(string p_cmdCommand) { return cmdall.Contains(p_cmdCommand.Trim().ToUpper().Split(' ').First()); }
+
+        private static List<string> cmdfolder = new List<string>(new string[] { "FOLDER" });
+        public static Boolean Folder(string p_cmdCommand) { return cmdfolder.Contains(p_cmdCommand.Trim().ToUpper().Split(' ').First()); }
+
+
+
+    }
+
+}
